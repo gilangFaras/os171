2fa82cba06a1ed40ff89a04d4cfbb612800eaf91
Gilang Faras Arifah - 1406623221 <gilang.faras@badak.cs.ui.ac.id>
2017-02-27 23:11:45 +0700

    exercise

---
+++ week02/exercise.c
@@ -0,0 +1,17 @@
+/*
+* Name: Gilang Faras
+* NPM: 1406623221
+* Class: A
+* Comment: maaf ya telat
+*/
+#define LOOP 3
+#include <stdio.h>
+void main() {
+ int input = 5;
+ int result, x;
+ for(x=0; x < LOOP; x++){
+   result += input;
+ }
+ printf("%d times %d equals %d \n", input, LOOP, result);
+}
+
